c5ce73225a98f4a73943424dfe4e4fca49f982cb
123redVersatester123_jniuninfggh <versatest123@gmail.com>
2026-07-22 17:10:28 +0900

    Fix greeting message in greet function
    Updated greeting message in greet function.

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 import pdb 
 def greet(name1):
     print(name1)
-    print("helloko,.,l,ook,ikoklk worldsdsdsdsdsd")
+    print("helloko,.,l,ook,iknjnnnnnoklk worldsdsdsdsdsd")
     return f"Hello, {name}!"return f"Hello, {name1}!"
 print(greet(";lsm;lmlsm;lmsalmlsamlm"))
